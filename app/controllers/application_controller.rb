6364a668142279d9577eedb9e3160fef6f130d57
Ziemowit Radogostowicz <ziemowit@MacBook-Pro-Ziemowit.local>
2018-08-28 00:31:04 +0200

    Layout by resource and better SASS file structure.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
   protected
+
+  def layout_by_resource
+    if devise_controller?
+      'auth'
+    else
+      'application'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
